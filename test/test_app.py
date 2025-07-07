c3a1bd19679da26ff702fe830cbd4e9930291799
Ranya <113499706+ranyabrkumar@users.noreply.github.com>
2025-07-08 03:14:26 +0530

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -1,12 +1,10 @@
 import pytest
-from app import home, app
-
+import app
 
 def test_home_route():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-   
 
 def test_not_found_route():
     tester = app.test_client()
