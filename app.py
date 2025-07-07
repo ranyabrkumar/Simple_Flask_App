97095f5a9b2cba559727a48996676bda547e9bd8
Ranya <113499706+ranyabrkumar@users.noreply.github.com>
2025-07-08 04:09:53 +0530

    removing test_client function from app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from Flask in Docker!"
-def test_client():
-    with app.test_client() as client:
-        response = client.get("/")
-        assert response.status_code == 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
